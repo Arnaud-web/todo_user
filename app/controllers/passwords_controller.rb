73853b9d550c81a681ad3543b7823f601b77dd82
Arnaud <mrandriamihaja2@gmail.com>
2019-01-12 14:21:04 +0300

    todo_user fin

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -24,9 +24,43 @@ class PasswordsController < ApplicationController
     end
 
 
+    def edit
+       
+        @user =User.find_by_id(params[:id])
+        if @user.recover_password != params[:token]
+
+            redirect_to new_password_path, danger: 'Token invalide'
+
+        end
 
 
+    end
 
 
+    def update
+        user_params =  params.require(:user).permit(:password, :password_confirmation, :recover_password)
+        @user =User.find(params[:id])
+        if @user.recover_password === user_params[:recover_password]
 
+            @user.assign_attributes(user_params)
+
+            if @user.valid?
+                @user.recover_password = nil
+                @user.save
+                session[:auth] = @user.to_session
+                redirect_to profil_path, success: 'Votre mot de passe a bien été modifier'
+
+            else
+                render :edit
+            end 
+
+        else
+
+            redirect_to new_password_path, danger: 'Token invalide'
+
+        end
+    end
+
+
+    
 end
